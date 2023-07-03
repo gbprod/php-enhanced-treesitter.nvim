310593a76b2ce20921f82721eccce2058042c0d4
gbprod <contact@gb-prod.fr>
2023-07-03 11:53:56 +0200

    fix: allow lowercase queries

--- queries/php/injections.scm
+++ queries/php/injections.scm
@@ -110,3 +110,114 @@
 ((heredoc value: (heredoc_body) @sql)
     (#lua-match? @sql "^%s*EXPLAIN")
     (#exclude_children! @sql))
+
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*select")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*update")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*delete")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*create")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*insert")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*replace")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*drop")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*alter")
+    (#exclude_children! @sql))
+(string (string_value) @sql
+    (#lua-match? @sql "^%s*explain")
+    (#exclude_children! @sql))
+
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*select")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*update")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*delete"))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*create")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*insert")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*replace")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*drop")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*alter")
+    (#exclude_children! @sql))
+(encapsed_string (string_value) @sql
+    (#lua-match? @sql "^%s*explain")
+    (#exclude_children! @sql))
+
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*select")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*update")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*delete")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*create")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*insert")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*replace")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*drop")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*alter")
+    (#exclude_children! @sql))
+((nowdoc value: (nowdoc_body) @sql)
+    (#lua-match? @sql "^%s*explain")
+    (#exclude_children! @sql))
+
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*select")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*update")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*delete")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*create")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*insert")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*replace")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*drop")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*alter")
+    (#exclude_children! @sql))
+((heredoc value: (heredoc_body) @sql)
+    (#lua-match? @sql "^%s*explain")
+    (#exclude_children! @sql))

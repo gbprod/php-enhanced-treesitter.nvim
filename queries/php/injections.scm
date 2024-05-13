93acad6b51750c15f022a27ab14b958b7f8173b6
gbprod <contact@gb-prod.fr>
2024-05-13 13:50:56 +0200

    fix: string queries

--- queries/php/injections.scm
+++ queries/php/injections.scm
@@ -1,176 +1,32 @@
 ;; extends
 
-(string (string_value) @injection.content
+(string_content) @injection.content
     (#set! injection.language "html")
     (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*</?%a*[%s*]?.*>"))
-
-(encapsed_string (string_value) @injection.content
-    (#set! injection.language "html")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*</?%a*[%s*]?.*>"))
+    (#lua-match? @injection.content "^%s*</?%a*[%s*]?.*>")
 
 (
     [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
+        (string_content) @injection.content
+        (nowdoc value: (nowdoc_body) @injection.content) 
+        (heredoc value: (heredoc_body) @injection.content) 
     ] 
     (#set! injection.language "sql")
     (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Ss][Ee][Ll][Ee][Cc][Tt]%s+.+[Ff][Rr][Oo][Mm]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Uu][Pp][Dd][Aa][Tt][Ee]%s+.+[Ss][Ee][Tt]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Dd][Ee][Ll][Ee][Tt][Ee]%s+[Ff][Rr][Oo][Mm]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Tt][Aa][Bb][Ll][Ee]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Ii][Nn][Dd][Ee][Xx]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Uu][Ss][Ee][Rr]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Dd][Rr][Oo][Pp]%s+[Ii][Nn][Dd][Ee][Xx]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Ff][Uu][Nn][Cc][Tt][Ii][Oo][Nn]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Ii][Nn][Ss][Ee][Rr][Tt]%s+[Ii][Nn][Tt][Oo]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Dd][Rr][Oo][Pp]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Tt][Rr][Uu][Nn][Cc][Aa][Tt][Ee]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Aa][Ll][Tt][Ee][Rr]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Ee][Xx][Pp][Ll][Aa][Ii][Nn]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Uu][Pp][Ss][Ee][Rr][Tt]%s+[Ii][Nn][Tt][Oo]"))
-
-(
-    [
-        (string (string_value) @injection.content)
-        (encapsed_string (string_value) @injection.content)
-        (nowdoc value: (nowdoc_body) @injection.content)
-        (heredoc value: (heredoc_body) @injection.content)
-    ] 
-    (#set! injection.language "sql")
-    (#set! injection.include-children)
-    (#lua-match? @injection.content "^%s*[Ww][Ii][Tt][Hh]%s+.*[Aa][Ss].*"))
+    (#any-lua-match? @injection.content 
+        "^%s*[Ss][Ee][Ll][Ee][Cc][Tt]%s+.+[Ff][Rr][Oo][Mm]"
+        "^%s*[Uu][Pp][Dd][Aa][Tt][Ee]%s+.+[Ss][Ee][Tt]"
+        "^%s*[Dd][Ee][Ll][Ee][Tt][Ee]%s+[Ff][Rr][Oo][Mm]"
+        "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Tt][Aa][Bb][Ll][Ee]"
+        "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Uu][Ss][Ee][Rr]"
+        "^%s*[Dd][Rr][Oo][Pp]%s+[Ii][Nn][Dd][Ee][Xx]"
+        "^%s*[Cc][Rr][Ee][Aa][Tt][Ee]%s+[Ff][Uu][Nn][Cc][Tt][Ii][Oo][Nn]"
+        "^%s*[Ii][Nn][Ss][Ee][Rr][Tt]%s+[Ii][Nn][Tt][Oo]"
+        "^%s*[Dd][Rr][Oo][Pp]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"
+        "^%s*[Tt][Rr][Uu][Nn][Cc][Aa][Tt][Ee]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"
+        "^%s*[Aa][Ll][Tt][Ee][Rr]%s+[Tt][Aa][Bb][Ll][Ee]%s+.*"
+        "^%s*[Ee][Xx][Pp][Ll][Aa][Ii][Nn]"
+        "^%s*[Uu][Pp][Ss][Ee][Rr][Tt]%s+[Ii][Nn][Tt][Oo].*"
+        "^%s*[Ww][Ii][Tt][Hh]%s+.*[Aa][Ss].*"
+    )
+)
